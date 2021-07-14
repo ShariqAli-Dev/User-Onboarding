3669f0b7c0cbdfd749765af3e3caa94c2442bbc3
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-14 13:05:23 -0400

    Completed Users.js

--- user-onboarding-app/src/components/Users.js
+++ user-onboarding-app/src/components/Users.js
@@ -2,7 +2,16 @@ export default function Users({ users }) {
   return (
     <div className="user-container">
       {users.map((user) => {
-        return <div key={user.name}>{user.name ? <h1>{user.name}</h1> : <h1>{user.first_name}</h1>}</div>;
+        if (!user) {
+          return <h2>Fetching user...</h2>;
+        }
+        return (
+          <div key={user.name}>
+            {!user.name ? <h1>{user.first_name}</h1> : <h2>{user.name}</h2>}
+            <p>Email: {user.email}</p>
+            <h3>{user.agreeToTerms}</h3>
+          </div>
+        );
       })}
     </div>
   );
