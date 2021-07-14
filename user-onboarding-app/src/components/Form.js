d753ca7ea0672ad5efe94863cf88017098ccc01b
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-14 18:13:32 -0400

    Completed Form.js   - Although I completed, props like 'error' are not yet functional   - Am going to edit App.js and see what parts I can finish

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -1,4 +1,54 @@
+import React from "react";
+
 export default function Form(props) {
-  const { values, submit } = props;
-  return <div>Hallo, this is form</div>;
+  const { values, submit, change, disabled, errors } = props;
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  return (
+    <form className="form container" onSubmit={onSubmit}>
+      {/* getting user information */}
+      <div className="form-group inputs">
+        <h4>User Information</h4>
+
+        {/* user text inputs */}
+        <label>
+          Name:
+          <input value={values.name} onChange={onChange} name="username" type="text" />
+          {/* name, email, password, agreeToTerms */}
+        </label>
+
+        <label>
+          Email:
+          <input value={values.email} onChange={onChange} name="email" type="text" />
+        </label>
+        {/* checkbox */}
+        <label>
+          Agree To Terms:
+          <input type="checkbox" name="agreeToTerms" checked={values.agreeToTerms} onChange={onChange} />
+        </label>
+      </div>
+
+      <div className="form-group submit">
+        <h2>Add User</h2>
+
+        {/* Disabling Button */}
+        <button disabled={disabled}>Submit</button>
+        <div className="errors">
+          <div>{errors.name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+          <div>{errors.agreeToTerms}</div>
+        </div>
+      </div>
+    </form>
+  );
 }
